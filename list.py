fce3461f6f65d4d40756ef14bf0d66aeed20ebe7
indrasupendisinaga <indrasupendisinaga@gmail.com>
2021-11-28 12:23:51 +0700

    penambahan materi

---
+++ list.py
@@ -0,0 +1,29 @@
+print ("------------------")
+print ("Pertemuan ke 9")
+print ("------------------")
+
+# Membuat list
+
+# a = [] #empty list
+# a = [1, 10, 6, 8] # list of integer
+# a = [1, "Hello", 6.3] # list with mixed datatype
+
+# Mengakses list
+
+# print (a[2]) # print element index 2
+# print (a[:2]) # print first element to index 2
+# print (a[2:4]) # print from index 2 to 4 (slide of list)
+
+# Mengubah element list
+# a = [2] = 10 # change the 2nd item
+# a = [2:4] = [1, 10, 6] # change 2nd to 4th items
+
+# Menambah element list
+
+# a.append(10) # and one item  to last element
+# a.extend([1, 10, 6]) # add several items to last element
+
+# Menggabungkan list
+
+# c = a + b # joint list a and b into c
+# a.extend(b) # add several items b into a
